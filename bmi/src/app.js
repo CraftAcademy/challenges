cbd6f7edd2d28b7706ebc7521731e45d226c1d06
tochman <thomas@craftacademy.se>
2017-08-06 01:42:32 +0200

    WIP RED refactors code to use deffered promise fails on UI tests

--- bmi/src/app.js
+++ bmi/src/app.js
@@ -1,5 +1,5 @@
-$(document).ready(function() {
-    loadLocale();
+var onReady = function(){
+  loadLocale().done(function(){
     $('#weight').focus(function() {
         $('#display_value').empty();
         $('#display_message').empty();
@@ -20,33 +20,48 @@ $(document).ready(function() {
         $('#display_value').html(i18n("bmi_message_prefix") + person.bmiValue);
         $('#display_message').html(i18n("bmi_message_results") + person.bmiMessage.toLowerCase());
     });
-});
-
-loadLocale = function(code) {
-    var url = new URL(window.location.href);
-    var locale = code || url.searchParams.get('locale') || 'en';
-    $.ajax(`src/locales/${locale}.json`)
-        .done(function(data) {
-            console.log(`parsed: ${locale}.json`)
-            i18n.translator.add(data);
-            $('h3').html(i18n('app_name'))
-            $('#calculate').html(i18n('calculate'))
-            $("#weight").attr("placeholder", i18n('weight'));
-            $("#height").attr("placeholder", i18n('height'));
-            $("#swedish").html(i18n('swedish'));
-            $("#english").html(i18n('english'));
-        }).fail(function() {
-            $('#display_value').html('Could not load translation file');
-        });
+  });
 }
 
-getLocaleCode = function(id){
-  switch (id) {
-      case 'swedish':
-          loadLocale('sv')
-          break;
-      case 'english':
-          loadLocale('en');
-          break;
-  }
+loadLocale = function(code) {
+    var deferred = $.Deferred();
+    setTimeout(function() {
+        var locale = code || 'en';
+        $.getJSON(`src/locales/${locale}.json`)
+            .done(function(data) {
+                i18n.translator.add(data);
+                applyTranstations();
+                // Resolve the deferred
+                deferred.resolve();
+            }).fail(function() {
+                $('#display_value').html('Could not load translation file');
+                deferred.reject();
+            });
+
+        // Resolve the deferred
+        deferred.resolve();
+    }, 200);
+    return deferred.promise();
+
+}
+
+getLocaleCode = function(id) {
+    switch (id) {
+        case 'swedish':
+            loadLocale('sv')
+            break;
+        case 'english':
+            loadLocale('en');
+            break;
+    }
+}
+
+applyTranstations = function() {
+    $('h3').html(i18n('app_name'))
+    $('#calculate').html(i18n('calculate'))
+    $("#weight").attr("placeholder", i18n('weight'));
+    $("#height").attr("placeholder", i18n('height'));
+    $("#swedish").html(i18n('swedish'));
+    $("#english").html(i18n('english'));
+    return;
 }

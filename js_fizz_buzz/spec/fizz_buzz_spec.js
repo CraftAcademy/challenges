e9f28e89a1d1d211a61b4d85ec269f353b6f31df
tochman <thomas@craftacademy.se>
2017-08-05 22:28:23 +0200

    adds data table to checkout challenge

--- js_fizz_buzz/spec/fizz_buzz_spec.js
+++ js_fizz_buzz/spec/fizz_buzz_spec.js
@@ -1,42 +1,39 @@
 describe('FizzBuzz', function () {
-    var game;
+  var game;
 
-   beforeEach(function () {
-       game = new FizzBuzz();
+  beforeEach(function () {
+    game = new FizzBuzz();
+  });
+
+  describe('knows when a number is', function () {
+    it('is divisable by 3', function () {
+        expect(game.divisibleByThree(3)).toBe(true)
     });
 
-   describe('knows when a number is', function () {
-
-        it('is divisable by 3', function () {
-            expect(game.divisibleByThree(3)).toBe(true)
-        });
-
-        it('is divisable by 5', function () {
-            expect(game.divisibleByFive(5)).toBe(true)
-        });
-
-        it('is divisable by 15', function () {
-            expect(game.divisibleByFifteen(15)).toBe(true)
-        });
-
+    it('is divisable by 5', function () {
+        expect(game.divisibleByFive(5)).toBe(true)
     });
 
-    describe('game returns', function () {
-        it('"Fizz" when number is divisible by 3', function () {
-            expect(game.fizz_buzz(3)).toEqual("Fizz");
-        });
+    it('is divisable by 15', function () {
+        expect(game.divisibleByFifteen(15)).toBe(true)
+    });
+  });
 
-        it('"Buzz" when number is divisible by 5', function () {
-            expect(game.fizz_buzz(5)).toEqual("Buzz");
-        });
-
-        it('"FizzBuzz" when number is divisible by 15', function () {
-            expect(game.fizz_buzz(15)).toEqual("FizzBuzz");
-        });
-
-        it('Returns number when no conditions are met', function() {
-           expect(game.fizz_buzz(1)).toEqual(1)
-        });
+  describe('game returns', function () {
+    it('"Fizz" when number is divisible by 3', function () {
+        expect(game.fizz_buzz(3)).toEqual("Fizz");
     });
 
+    it('"Buzz" when number is divisible by 5', function () {
+        expect(game.fizz_buzz(5)).toEqual("Buzz");
+    });
+
+    it('"FizzBuzz" when number is divisible by 15', function () {
+        expect(game.fizz_buzz(15)).toEqual("FizzBuzz");
+    });
+
+    it('Returns number when no conditions are met', function() {
+       expect(game.fizz_buzz(1)).toEqual(1)
+    });
+  });
 });

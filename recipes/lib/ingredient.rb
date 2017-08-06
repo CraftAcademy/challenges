4f0e1ead736b835c0e6704e5d7675c7ecf18a8d8
tochman <thomas@craftacademy.se>
2017-08-06 09:49:12 +0200

    Revert "adds source code for recipes challenge"
    This reverts commit fa17dfcaeb48138b80ebd7270061e65baea926f3.

--- recipes/lib/ingredient.rb
+++
@@ -1,9 +0,0 @@
-class Ingredient
-  attr_accessor :amount, :name, :unit
-
-  def initialize(amount, unit, name)
-    @amount = amount
-    @name = name
-    @unit = unit
-  end
-end
--- recipes/lib/step.rb
+++
@@ -1,8 +0,0 @@
-class Step
-  attr_accessor :position, :text
-
-  def initialize(position, text)
-    @position = position
-    @text = text
-  end
-end
